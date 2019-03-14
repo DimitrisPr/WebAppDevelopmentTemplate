55b06ae00efd1d19961a3adcdff7dabaef4cbc38
root <dimitris@prasakis.com>
2019-03-14 22:57:37 +0200

    Everything's setup, ready to begin

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-loll
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="author" content="">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="public/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div id="login">
+    </div>
+
+    <!-- <script src="./public/js/jquery.js"></script> -->
+    <!-- <script src="./public/js/script.js"></script> -->
+</body>
+
+</html>
